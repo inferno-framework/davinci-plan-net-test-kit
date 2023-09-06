efd126b3965bc1ab8eb7f7ca6f6edd6568500eb2
Karl Naden <knaden@mitre.org>
2023-09-06 08:24:43 -0400

    Generated updates for parameterless gathering

--- lib/davinci_pdex_plan_net_test_kit/generated/v1.1.0/plannet_organization/plannet_organization_no_params_search_test.rb
+++ lib/davinci_pdex_plan_net_test_kit/generated/v1.1.0/plannet_organization/plannet_organization_no_params_search_test.rb
@@ -6,7 +6,7 @@ module DaVinciPDEXPlanNetTestKit
     class PlannetOrganizationNoParamsSearchTest < Inferno::Test
       include DaVinciPDEXPlanNetTestKit::SearchTest
 
-      title 'Server returns valid results for Organization search without filters'
+      title 'Client can identify instances of Plan-Net Organization on the server'
       description %(
 A server SHALL support searching on the 
 Organization resource withough parameters. This test
@@ -21,6 +21,14 @@ response will be used for subsequent tests.
       )
 
       id :davinci_pdex_plan_net_v110_plannet_organization_no_params_search_test
+
+      input :davinci_pdex_plan_net_v110_plannet_organization_no_params_search_test_ids,
+        title: 'Ids of instances of Plan-Net Organization',
+        optional: true,
+        description: 'Required if parameterless searches not used.'
+      input :no_param_search
+      input :max_pages
+
       def self.properties
         @properties ||= SearchTestProperties.new(
           first_search: true,
@@ -39,7 +47,7 @@ response will be used for subsequent tests.
       end
 
       run do
-        run_search_no_params_test
+        run_search_no_params_test(davinci_pdex_plan_net_v110_plannet_organization_no_params_search_test_ids)
       end
     end
   end
