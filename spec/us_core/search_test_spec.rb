0b0edb498016af2bca47da0fe15bfcc6f942ad7b
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-06-11 17:21:59 -0400

    rm local run methods

--- spec/us_core/search_test_spec.rb
+++ spec/us_core/search_test_spec.rb
@@ -1,23 +1,7 @@
-RSpec.describe DaVinciPlanNetTestKit::SearchTest do
-  let(:suite) { Inferno::Repositories::TestSuites.new.find('davinci_plan_net_server_v110') }
-  let(:session_data_repo) { Inferno::Repositories::SessionData.new }
-  let(:test_session) { repo_create(:test_session, test_suite_id: suite.id) }
+RSpec.describe DaVinciPlanNetTestKit::SearchTest, :runnable do
+  let(:suite_id) { 'davinci_plan_net_server_v110' }
   let(:url) { 'http://example.com/fhir' }
   let(:error_outcome) { FHIR::OperationOutcome.new(issue: [{ severity: 'error' }]) }
-
-  def run(runnable, inputs = {})
-    test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
-    test_run = Inferno::Repositories::TestRuns.new.create(test_run_params)
-    inputs.each do |name, value|
-      session_data_repo.save(
-        test_session_id: test_session.id,
-        name: name,
-        value: value,
-        type: runnable.config.input_type(name)
-      )
-    end
-    Inferno::TestRunner.new(test_session: test_session, test_run: test_run).run(runnable)
-  end
 
   describe '_include search' do
     let (:include_test) do 
